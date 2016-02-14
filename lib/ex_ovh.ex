9f0a22b037498591cc62477cd71c9bd81203c5f1
Stephen Moloney <stephenmoloney@live.com>
2016-02-14 12:44:12 +0000

    don't start exovh as an application. user adds manually to supervisor tree.

--- lib/ex_ovh.ex
+++ lib/ex_ovh.ex
@@ -1,13 +1,4 @@
 defmodule ExOvh do
+  @moduledoc :false
   use ExOvh.Client, otp_app: :ex_ovh
-
-  # <<TODO>> Remove application later so that ExOvh is started within a supervision tree on demand
-  use Application
-
-  def start(_type, _args) do
-    LoggingUtils.log_mod_func_line(__ENV__, :debug)
-    __MODULE__.start_link()
-  end
-
-
 end
